fd9f01be361ce287ee1ca019b3b2aa621e8819a9
David Wu <dwu97@yahoo.com>
2020-06-07 13:29:13 -0400

    Clarify distinction from tm

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-import Link from "@material-ui/core/Link";
-
 import MealTimeButtons from "../meal-time-buttons/meal-time-buttons.component";
 import MealDayButtons from "../meal-day-buttons/meal-day-buttons.component";
+
+import tmReactLogo from "../../assets/tm-react.jpg";
 
 import "./header.styles.scss";
 
 const Header = ({mealTime, mealDay, setMealTime, setMealDay}) => (
   <header className="header">
-    <h1 className="title"><a href="/">TigerMenus</a></h1>
+    <h1 className="title">
+      <img src={tmReactLogo} width="50" alt=""/>
+      <a href="/">TigerMenus React</a>
+    </h1>
     <a href="https://github.com/axu2/tiger-menus" className="code-link">
-      Code (GitHub)
+      TigerMenus Code (GitHub)
     </a>
     <MealTimeButtons mealTime={mealTime} setMealTime={setMealTime} className="meal-time-buttons" />
     <MealDayButtons mealDay={mealDay} setMealDay={setMealDay} className="meal-day-buttons" />
