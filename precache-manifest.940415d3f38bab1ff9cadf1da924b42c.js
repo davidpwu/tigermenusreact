fbe9c39a8872d60dd785b3ca9bdda47a1f0f55ad
David Wu <dwu97@yahoo.com>
2020-06-07 13:16:01 -0400

    Updates

---
+++ precache-manifest.940415d3f38bab1ff9cadf1da924b42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a70bc2d4ace36174ecbc7f9fec84fd",
+    "url": "/davidpwu/tigermenusreact/index.html"
+  },
+  {
+    "revision": "98fdd49452f8ef01b899",
+    "url": "/davidpwu/tigermenusreact/static/css/main.2a334bbd.chunk.css"
+  },
+  {
+    "revision": "150470e40dd9c2063346",
+    "url": "/davidpwu/tigermenusreact/static/js/2.27e4bc20.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/davidpwu/tigermenusreact/static/js/2.27e4bc20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98fdd49452f8ef01b899",
+    "url": "/davidpwu/tigermenusreact/static/js/main.df564d38.chunk.js"
+  },
+  {
+    "revision": "e632dda72229b30fd220",
+    "url": "/davidpwu/tigermenusreact/static/js/runtime-main.1010ce8b.js"
+  }
+]);
